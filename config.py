386330593969a0f3040c2a8a43017cf341041869
techy-tresa <tejaswaninyak1122@gmail.com>
2026-03-17 01:31:56 +0530

    Refactor database URI to use BASE_DIR
    Updated SQLALCHEMY_DATABASE_URI to use BASE_DIR for path.

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-change-this-in-production'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/civicconnect.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'civicconnect.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
