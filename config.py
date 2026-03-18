a370855fba760f87abf7a24eb3e26a5ffc64f5f1
techy-tresa <tejaswaninyak1122@gmail.com>
2026-03-18 11:13:59 +0530

    Update SECRET_KEY in config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-change-this-in-production'
+    SECRET_KEY = 'dev-key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'civicconnect.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
