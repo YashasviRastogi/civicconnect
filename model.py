7558311bc248d3ef0c01d0b575fba3883d717b11
YashasviRastogi <yashasvirastogi007@gmail.com>
2026-03-19 13:36:21 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-
+ 
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
